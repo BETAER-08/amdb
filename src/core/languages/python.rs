7234b221bfc616ed1265097d6447ccef6226fa52
betaer <bataer08@gmail.com>
2026-01-22 23:41:10 +0900

    languages

---
+++ src/core/languages/python.rs
@@ -0,0 +1,12 @@
+use tree_sitter::Language;
+
+pub fn language() -> Language {
+    tree_sitter_python::LANGUAGE.into()
+}
+
+pub const QUERY: &str = r#"
+    (function_definition name: (identifier) @Function)
+    (class_definition name: (identifier) @Class)
+    (call function: (identifier) @Call)
+    (call function: (attribute attribute: (identifier) @Call))
+"#;
