3c9b88ec754829f6e67ca5ea292bff89627c21b1
betaer <bataer08@gmail.com>
2026-01-26 20:55:53 +0900

    ruby

---
+++ src/core/languages/ruby.rs
@@ -0,0 +1,11 @@
+use tree_sitter::Language;
+
+pub fn language() -> Language {
+    tree_sitter_ruby::language()
+}
+
+pub const QUERY: &str = r#"
+(class name: (constant) @name) @class
+(module name: (constant) @name) @module
+(method name: (identifier) @name) @method
+"#;
