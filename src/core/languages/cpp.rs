907a45164c6f3c45f8f854aa5e2c61257c5f4188
betaer <bataer08@gmail.com>
2026-01-26 20:54:21 +0900

    cpp

---
+++ src/core/languages/cpp.rs
@@ -0,0 +1,12 @@
+use tree_sitter::Language;
+
+pub fn language() -> Language {
+    tree_sitter_cpp::language()
+}
+
+pub const QUERY: &str = r#"
+(function_definition declarator: (function_declarator declarator: (identifier) @name)) @function
+(class_specifier name: (type_identifier) @name) @class
+(struct_specifier name: (type_identifier) @name) @struct
+(template_declaration (class_specifier name: (type_identifier) @name)) @class
+"#;
