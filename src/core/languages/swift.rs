80099951dfe6151b5cfc451194eed17b41ded58b
betaer <bataer08@gmail.com>
2026-01-26 20:56:12 +0900

    swift

---
+++ src/core/languages/swift.rs
@@ -0,0 +1,12 @@
+use tree_sitter::Language;
+
+pub fn language() -> Language {
+    tree_sitter_swift::language()
+}
+
+pub const QUERY: &str = r#"
+(class_declaration name: (type_identifier) @name) @class
+(struct_declaration name: (type_identifier) @name) @struct
+(protocol_declaration name: (type_identifier) @name) @interface
+(function_declaration name: (simple_identifier) @name) @function
+"#;
