7cc9342ca87a3fe197c47cd2dbb621247f4df39e
betaer <bataer08@gmail.com>
2026-01-26 20:54:06 +0900

    c

---
+++ src/core/languages/c.rs
@@ -0,0 +1,11 @@
+use tree_sitter::Language;
+
+pub fn language() -> Language {
+    tree_sitter_c::language()
+}
+
+pub const QUERY: &str = r#"
+(function_definition declarator: (function_declarator declarator: (identifier) @name)) @function
+(struct_specifier name: (type_identifier) @name) @struct
+(enum_specifier name: (type_identifier) @name) @enum
+"#;
