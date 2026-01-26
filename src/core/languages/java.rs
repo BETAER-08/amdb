22f8f904e117f8d458a1ef7673844f6db3c68e8b
betaer <bataer08@gmail.com>
2026-01-26 20:54:38 +0900

    java

---
+++ src/core/languages/java.rs
@@ -0,0 +1,12 @@
+use tree_sitter::Language;
+
+pub fn language() -> Language {
+    tree_sitter_java::language()
+}
+
+pub const QUERY: &str = r#"
+(class_declaration name: (identifier) @name) @class
+(interface_declaration name: (identifier) @name) @interface
+(method_declaration name: (identifier) @name) @method
+(enum_declaration name: (identifier) @name) @enum
+"#;
